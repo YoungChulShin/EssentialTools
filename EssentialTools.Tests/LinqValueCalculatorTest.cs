c1fb361142d1398f12b0bffdf6f828f900cc8da2
YoungChulShin <go1323@gmail.com>
2019-06-11 06:54:16 +0900

    [Moq] 간단한 Moq 구현

--- EssentialTools.Tests/LinqValueCalculatorTest.cs
+++ EssentialTools.Tests/LinqValueCalculatorTest.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using EssentialTools.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace EssentialTools.Tests
 {
@@ -20,15 +21,24 @@ namespace EssentialTools.Tests
         public void Sum_Products_Correctly()
         {
             // Arrange
-            var discounter = new MinimumDiscountHelper();
-            var target = new LinqValueCalculator(discounter);
-            var goalTotal = products.Sum(e => e.Price);
+
+            //var discounter = new MinimumDiscountHelper();
+            //var target = new LinqValueCalculator(discounter);
+            //var goalTotal = products.Sum(e => e.Price);
+
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();   // IDiscounterHelper 인터페이스를 구현하는 Mock 개체를 사용하려고 한다
+
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))   // LinqValueCalculator 테스트를 위한 ApplyDiscount 메서드를 선택
+                                                                    // It 클래스를 이용해서 매개변수의 값을 설정 
+                                                                    // IsAny는 모든 T형식의 값을 지. 이 외에도 조건이나 범위 설정이 가능
+                .Returns<decimal>(total => total);  // ApplyDiscount의 결과 값을 리턴
+            var target = new LinqValueCalculator(mock.Object);
 
             // Act
             var result = target.ValueProducts(products);
 
             // Assert
-            Assert.AreEqual(goalTotal, result);
+            Assert.AreEqual(products.Sum(e => e.Price), result);
         }
     }
 }
